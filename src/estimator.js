15b5ae3281b5202da836409e90c28722f44e8bb4
ampaire <femayapr@gmail.com>
2020-04-12 15:39:08 -0600

    Worked on challenge one

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,18 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const { reportedCases, timeToElapse } = data;
+  const output = Math.floor(timeToElapse / 3);
+
+  return {
+    data,
+    impact: {
+      currentlyInfected: reportedCases * 10,
+      infectionsByRequestedTime: reportedCases * 10 * 2 ** output
+    },
+    severeImpact: {
+      currentlyInfected: reportedCases * 50,
+      infectionsByRequestedTime: reportedCases * 50 * 2 ** output
+    }
+  };
+};
 
 export default covid19ImpactEstimator;
